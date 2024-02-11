b2593bf15f414fcfd68e159627cccd3ec764d4c5
PoH-rull <101949468+PoH-rull@users.noreply.github.com>
2024-02-11 15:17:55 +0200

    added utils classes

---
+++ crescendo -official robot code/src/main/java/frc/robot/utiils/SparkFlexMotor.java
@@ -0,0 +1,11 @@
+package frc.robot.utiils;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class SparkFlexMotor extends CanSparkMotor {
+
+    public SparkFlexMotor(int canID) {
+        super(new CANSparkFlex(canID, MotorType.kBrushless));
+    }
+}
